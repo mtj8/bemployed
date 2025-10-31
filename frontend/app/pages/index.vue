8ff94ded6ccb92b5a039f59dd5891e8010cc1dc8
Kenneth Ng <145921389+kennething@users.noreply.github.com>
2025-10-31 11:17:05 -0400

    add daisyui and more config

--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="flex h-screen w-screen flex-col items-center justify-center">
     <h1 class="text-5xl font-bold">whats a shower</h1>
+    <p>fgiosahgisdghudsiu</p>
   </div>
 </template>
 
