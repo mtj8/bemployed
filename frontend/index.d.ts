9afa0c4efcdc586b939feb9b68e58b6f5a654a98
Kenneth Ng <145921389+kennething@users.noreply.github.com>
2025-11-22 16:32:54 -0500

    Update index.d.ts

--- frontend/index.d.ts
+++ frontend/index.d.ts
@@ -1,7 +1,6 @@
 // https://nuxt.com/docs/guide/directory-structure/pages#typing-custom-metadata
 
 declare module "#app" {
-   
   interface PageMeta {
     /** Whether or not the page requires authentication. */
     requiresAuth: boolean;
